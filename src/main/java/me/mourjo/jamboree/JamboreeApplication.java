86776c62380d61dd8c21f3ab8d7e44439f816db2
Mourjo Sen <hello@mourjo.me>
2023-08-13 11:33:01 +0200

    Initial Commit

---
+++ src/main/java/me/mourjo/jamboree/JamboreeApplication.java
@@ -0,0 +1,13 @@
+package me.mourjo.jamboree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamboreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamboreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/mourjo/jamboree/JamboreeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mourjo.jamboree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamboreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
