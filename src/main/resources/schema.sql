4616c66837a1a2321ef65411c6616a8e610e117f
oitee <oitee.codes@gmail.com>
2024-03-09 18:02:36 +0530

    Add changes to make Github Actions work on postgress DB

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,22 +1,12 @@
-\c postgres;
-DROP DATABASE IF EXISTS jamboree;
-CREATE DATABASE jamboree;
-\connect jamboree;
+--Follow these steps for manual setup:
+--\c postgres;
+--DROP DATABASE IF EXISTS jamboree;
+--CREATE DATABASE jamboree;
+--\connect jamboree;
+--DROP TABLE party IF EXISTS;
 
-CREATE TABLE party (
-id SERIAL,
-name TEXT,
-location TEXT,
-party_time TIMESTAMP WITH TIME ZONE,
-created_at TIMESTAMP WITH TIME ZONE);
-
---TEST DATABASE--
-DROP DATABASE IF EXISTS jamboreetest;
-CREATE DATABASE jamboreetest;
-\connect jamboreetest;
-
-CREATE TABLE party (
-id SERIAL,
+CREATE TABLE IF NOT EXISTS party (
+id SERIAL PRIMARY KEY NOT NULL,
 name TEXT,
 location TEXT,
 party_time TIMESTAMP WITH TIME ZONE,
